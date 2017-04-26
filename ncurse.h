cc9bf0c4d353db102ce0b45291ad7c26f647c83f
Cruaver <charbel.kabro@gmail.com>
2017-04-26 09:46:14 +0200

    test

--- ncurse.h
+++ ncurse.h
@@ -6,6 +6,6 @@
 
 enum Direction get_next_move(enum Direction previous);
 
-void display_points(PointList *snake, const chtype symbol);
+void display_points(PointList *snake, const chtype symbol, WINDOW * boite);
 
 #endif
