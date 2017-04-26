e644d2e308ef814225d0a4520afeff8af3c15c0d
Cruaver <charbel.kabro@gmail.com>
2017-04-26 09:48:03 +0200

    test

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]) {
     curs_set(0);
     timeout(10);
     getmaxyx(stdscr, ymax, xmax);
-    boite = subwin(stdscr, LINES, COLS, 0, 0);
+    boite = subwin(stdscr, LINES - 2, COLS - 2, 0, 0);
     wborder(boite, '|', '|', '-', '-', '+', '+', '+', '+');
     wrefresh(boite);
 
