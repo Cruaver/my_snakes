45cdc25c0347e6e1fc97d1c2777cd007f2dcca5b
Cruaver <charbel.kabro@gmail.com>
2017-04-26 09:28:05 +0200

    test

--- test/test3.c
+++ test/test3.c
@@ -3,27 +3,19 @@
 
 int main(int argc, char *argv[]) {
 
-    WINDOW *haut, *bas;
+    WINDOW *boite;
 
     initscr();
-    haut= subwin(stdscr, LINES / 2, COLS, 0, 0);        // Créé une fenêtre de 'LINES / 2' lignes et de COLS colonnes en 0, 0
-    bas= subwin(stdscr, LINES / 2, COLS, LINES / 2, 0); // Créé la même fenêtre que ci-dessus sauf que les coordonnées changent
+    boite= subwin(stdscr, 10, 10, LINES / 2, COLS /2);
 
-    box(haut, ACS_VLINE, ACS_HLINE);
-    box(bas, ACS_VLINE, ACS_HLINE);
-
-    mvwprintw(haut, 1, 1, "Ceci est la fenetre du haut");
-    mvwprintw(bas, 1, 1, "Ceci est la fenetre du bas");
-
-    wrefresh(haut);
-    wrefresh(bas);
+    box(boite, ACS_VLINE, ACS_HLINE); // ACS_VLINE et ACS_HLINE sont des constantes qui génèrent des bordures par défaut
+    refresh();
 
     getch();
     endwin();
 
-    free(haut);
-    free(bas);
-
+    free(boite);
 
     return 0;
+
 }
