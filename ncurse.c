548e8f87b43a84a24a4daba517e06da81e8a1afb
Cruaver <charbel.kabro@gmail.com>
2017-05-04 14:00:30 +0200

    test

--- ncurse.c
+++ ncurse.c
@@ -14,18 +14,26 @@ enum Direction get_next_move(enum Direction previous) {
     int ch;
 
     ch = getch();
-    if (ch == KEY_LEFT) {
+    if (ch == KEY_LEFT)
+    {
         if (previous != RIGHT)
             return LEFT;
-    } else if (ch == KEY_RIGHT) {
+    }
+    else if (ch == KEY_RIGHT)
+    {
         if (previous != LEFT)
             return RIGHT;
-    } else if (ch == KEY_DOWN) {
+    }
+    else if (ch == KEY_DOWN)
+    {
         if (previous != UP)
             return DOWN;
-    } else if (ch == KEY_UP) {
+    }
+    else if (ch == KEY_UP)
+    {
         if (previous != DOWN)
             return UP;
-    } else
+    }
+    else
         return previous;
 }
