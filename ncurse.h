5eb6dc917445288dd6902a9d1e185803cf3a48d5
Cruaver <charbel.kabro@gmail.com>
2017-05-04 14:29:01 +0200

    test

--- ncurse.h
+++ ncurse.h
@@ -2,11 +2,10 @@
 # define NCURSE_H_
 
 #include <ncurses.h>
-#include "game.h"
 
 
 enum Direction get_next_move(enum Direction previous);
 
-int display_points(PointList *snake, const chtype symbol);
+void display_points(PointList *snake, const chtype symbol);
 
 #endif
