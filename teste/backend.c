7ee0b594cff106fe89309a7148585076b587591a
Cruaver <charbel.kabro@gmail.com>
2017-05-04 15:05:57 +0200

    test

--- teste/backend.c
+++ teste/backend.c
@@ -25,27 +25,19 @@ bool remove_from_list(PointList* elt, PointList** list) {
 }
 
 enum Status move_snake(Board* board, enum Direction dir) {
-    // Create a new beginning. Check boundaries.
     PointList* beginning = next_move(board, dir);
     if (beginning == NULL) {
         return FAILURE;
     }
-
-    // If we've gone backwards, don't do anything
     if (board->snake->next && is_same_place(beginning, board->snake->next)) {
         beginning->next = NULL;
         free(beginning);
         return SUCCESS;
     }
-
-    // Check for collisions
     if (list_contains(beginning, board->snake)) {
         return FAILURE;
     }
-
-    // Check for food
     if (list_contains(beginning, board->foods)) {
-        // Attach the beginning to the rest of the snake;
         beginning->next = board->snake;
         board->snake = beginning;
         remove_from_list(beginning, &(board->foods));
@@ -53,13 +45,8 @@ enum Status move_snake(Board* board, enum Direction dir) {
 
         return SUCCESS;
     }
-
-    // Attach the beginning to the rest of the snake
     beginning->next = board->snake;
     board->snake = beginning;
-
-
-    // Cut off the end
     PointList* end = board->snake;
     while(end->next->next) {
         end = end->next;
